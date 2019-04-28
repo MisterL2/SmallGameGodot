871fbc2af93c96ae790430628c28a9c5d7f5aee7
MisterL2 <46602881+MisterL2@users.noreply.github.com>
2019-04-28 12:29:21 +0200

    v0.5.1 (Visual improvements)
    -BIG visual improvement to the Main Menu
    -Difficulty can no longer be changed to what it was before
    -Item hitboxes (and the health sprite) were made smaller

--- Godot/AnimatedSprite.gd
+++ Godot/AnimatedSprite.gd
@@ -1,11 +1,9 @@
 extends AnimatedSprite
 
 func animateDeath():
-	print("animation started!")
 	self.animation = "DeathFade"
 	self.playing = true
 
 
-func _on_animation_finished():
-	print("animation finished!")
+func _on_animation_finished(): # Never called apparently?
 	emit_signal("animation_finished")
--- Godot/DifficultyButton.gd
+++ Godot/DifficultyButton.gd
@@ -1,4 +1,15 @@
 extends Button
 
+func _ready():
+	call_deferred("self_press")
+	
+func self_press():
+	if self.name == $"/root/Base".DIFFICULTY:
+		self.grab_focus()
+		self.disabled = true
+
 func _on_pressed():
-	get_node("/root/MainMenu").changeDifficulty($Label.text)
+	$"/root/MainMenu".changeDifficulty($Label.text)
+	for button in self.get_parent().get_children():
+		button.disabled = false
+	self.disabled = true
